bbf1f3da5368f3310a94906fcefc99ca3356b181
Truc Duong <trucduong@tmon.co.kr>
2018-08-12 17:33:32 +0700

    remove example

--- example/src/main/java/com/green/example/config/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.green.example.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
--- example/src/main/java/com/green/example/entities/RoleName.java
+++
@@ -1,6 +0,0 @@
-package com.green.example.entities;
-
-public enum RoleName {
-    USER,
-    ADMIN
-}
